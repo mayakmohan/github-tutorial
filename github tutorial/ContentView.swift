aff6dc47b7d73a264afb2dd6774fe23906f5aad9
Maya Mohan <mohanmayak@gmail.com>
2024-07-12 12:48:56 -0700

    Initial Commit

---
+++ github tutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  github tutorial
+//
+//  Created by Smita Mohan on 7/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ github tutorial/github_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_tutorialApp.swift
+//  github tutorial
+//
+//  Created by Smita Mohan on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct github_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
